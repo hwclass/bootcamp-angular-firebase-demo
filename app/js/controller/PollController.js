aa2058a67d5a2bf229a901fba89eb6d7acdb9108
hwclass <harderworking.class@gmail.com>
2014-11-23 04:08:43 +0200

    Added the data strcuture.

--- app/js/controller/PollController.js
+++ app/js/controller/PollController.js
@@ -12,8 +12,11 @@ define(function () {
     
     var sync = $firebase(ref);
     
-    $scope.data = sync.$asObject();
+    var comingData = sync.$asArray();
 
+    $scope.answers = comingData;
+
+    /*
   	$scope.data.$loaded().then(function() {
     	$scope.answers = (function() {
         return $scope.data.answers.answers;
@@ -22,21 +25,36 @@ define(function () {
         return $scope.data.answers.questionaires;
     	})();
   	});
+    */
 
     $scope.submitPollForm = function () {
+      
+      console.log($scope.choice);
+
+      var takinardi = sync._arraySync.getArray().$save({_id: $scope.choice, vote : "444"}).then(function() {
+        console.log('Oyunuz kaydedildi. Teşekkürler.');
+      });
+
       var item = null;
+      
+      /*
       for (var counterForItems = 0, len = $scope.data.answers.answers.length; counterForItems < len; counterForItems++) {
         if ($scope.choice === $scope.data.answers.answers[counterForItems]['_id']) {
           item = $scope.data.answers.answers[counterForItems];
         }
       }
+
       console.dir(item);
       console.dir(sync._objectSync.getObject().answers.answers);
       console.dir(ref.child("foo/counter"));
-
+      */
+      
+      /*
       if (sync._objectSync.getObject().answers.answers[item._id] == item._id) {
         console.log('sadasdasd');
-      } 
+      }
+      */
+
     }
     
   }];
